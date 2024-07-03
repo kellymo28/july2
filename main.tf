7f4775405ec402b46b22dea4ec06a95f74cff008
kellymo28 <kelly.morali@hashicorp.com>
2024-07-03 11:37:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,8 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.1"
 }
+
+module "s3-bucket-2" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
+}
